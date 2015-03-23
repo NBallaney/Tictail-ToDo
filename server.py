31f2c4fee516ede6de33d4917275b40c980ba81a
Nikhil Ballaney <nballaney@gmail.com>
2015-03-22 17:36:41 -0700

    Convert to todo app

--- server.py
+++ server.py
@@ -1,22 +1,22 @@
 import json
 from flask import Flask, Response, request
 
-app = Flask(__name__, static_url_path='', static_folder='public')
+app = Flask(__name__, static_url_path='', static_folder='client')
 app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
 
-@app.route('/comments.json', methods=['GET', 'POST'])
-def comments_handler():
+@app.route('/tasks.json', methods=['GET', 'POST'])
+def tasks_handler():
 
-    with open('comments.json', 'r') as file:
-        comments = json.loads(file.read())
+    with open('tasks.json', 'r') as file:
+        tasks = json.loads(file.read())
 
     if request.method == 'POST':
-        comments.append(request.form.to_dict())
+        tasks.append(request.form.to_dict())
 
-        with open('comments.json', 'w') as file:
-            file.write(json.dumps(comments, indent=4, separators=(',', ': ')))
+        with open('tasks.json', 'w') as file:
+            file.write(json.dumps(tasks, indent=4, separators=(',', ': ')))
 
-    return Response(json.dumps(comments), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
+    return Response(json.dumps(tasks), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
 
 if __name__ == '__main__':
     app.run(port=3000)
